3b479eeeab006eed70f57b62c4475f005f85b95d
bucirina <bucirina@yandex.ru>
2021-12-09 21:53:50 +0300

    Some changes

--- sandbox/src/main/java/ru/stqa/train/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/train/sandbox/MyFirstProgram.java
@@ -4,23 +4,18 @@ public class MyFirstProgram {
 
   public static void main (String[] args) {
      hello("world");
+     Square s = new Square (5);
+     System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-     double len = 5;
-      System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+Rectangle r = new Rectangle(4, 6);
 
-      double a = 4;
-      double b = 6;
-      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b  + " = " + area(a, b));
+      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b  + " = " + r.area());
     }
   public static void hello ( String somebody)
   {
 
       System.out.println("Hello, " + somebody + "!");
   }
-public static double area (double l) {
-      return l * l;
-}
-public static double area (double a, double b) {
-      return a * b;
-}
+
+
 }
